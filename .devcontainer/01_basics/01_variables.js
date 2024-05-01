72f17c6ad173ac6f4c37fbb2b7a6d1e6a9c7fefa
pankajkumar0870 <168636625+pankajkumar0870@users.noreply.github.com>
2024-05-01 12:11:04 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1221265
+let accountEmail = "me.pankaj918@gmail.accountEmail"
+var accountPassword = "1221266"
+accountCity = "Jaipur"
+
+accountEmail = "me.pankaj@gmail.com"
+accountPassword = "12212688"
+accountCity = "Bengaluru"
+// accountId = 2
+
+ console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
