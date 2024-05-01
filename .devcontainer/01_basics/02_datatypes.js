b2c3ffa2842b87d2ee57754f117ec8d8c9d391eb
pankajkumar0870 <168636625+pankajkumar0870@users.noreply.github.com>
2024-05-01 12:09:35 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all javascript as a newer version
+// alert(3+3) // we are using node js , not browser
+
+let name = "pankaj"
+let age = 21
+let isLoggedIn = false
+
+// num => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false 
+// null => stand alone value 
+// undefined
+//symbol => unique
+
+//object
+console.log(typeof age);
+
+
+
