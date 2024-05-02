fa4291c44d8dbe4f53b7a1ae6f4995e8afa559d8
pankajkumar0870 <168636625+pankajkumar0870@users.noreply.github.com>
2024-05-02 13:16:00 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOPeration.js
@@ -0,0 +1,35 @@
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log( valueInNumber);
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log( valueInNumber);
+
+// let score = true  //false =>0
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log (valueInNumber);
+
+// let  weTakeNumber = 0 //name =>true //"" =>false 
+// let booleanweTakeNumber = Boolean(weTakeNumber)
+// console.log(booleanweTakeNumber);
